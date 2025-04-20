91a8ef6517b8b03e66b4a210cd8a5fbda97b8fcb
Ishmael Jup <jupiterishmael@gmail.com>
2025-04-20 20:28:54 +0000

    Fixed login

--- App/views/index.py
+++ App/views/index.py
@@ -2,11 +2,11 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
-
 @index_views.route('/', methods=['GET'])
 def index_page():
     google_maps_api_key = current_app.config['GOOGLE_MAPS_API_KEY']
-    return render_template('layout.html', google_maps_api_key=google_maps_api_key)
+    return render_template('index.html', google_maps_api_key=google_maps_api_key, show_map=True)
+
 
 @index_views.route('/init', methods=['GET'])
 def init():
